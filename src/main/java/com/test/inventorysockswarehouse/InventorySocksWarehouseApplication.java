87f68dd7a192b76a96088aada15e2fd297784aaf
Egorkka74 <Egorkka7401@gmail.com>
2024-12-18 10:17:09 +0500

    init

---
+++ src/main/java/com/test/inventorysockswarehouse/InventorySocksWarehouseApplication.java
@@ -0,0 +1,12 @@
+package com.test.inventorysockswarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventorySocksWarehouseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(InventorySocksWarehouseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/test/inventorysockswarehouse/InventorySocksWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.inventorysockswarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventorySocksWarehouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
